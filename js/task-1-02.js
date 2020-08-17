27781a32da90187cc1f8fac271022fb441a83ef0
Pavlenko-Oksana <65184570+Pavlenko-Oksana@users.noreply.github.com>
2020-08-17 17:15:59 +0300

    добавляет докуметы для работы с дз 2

---
+++ js/task-1-02.js
@@ -0,0 +1,12 @@
+const logItems = function(array) {
+    for(let i=0; i<logItems.length -1; i +=1) {
+        console.log(logItems[i])
+    }
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+  
+  logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
