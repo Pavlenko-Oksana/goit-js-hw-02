93c8bce95ee5e5540312c2b6534ab27bc3200530
Pavlenko-Oksana <65184570+Pavlenko-Oksana@users.noreply.github.com>
2020-08-19 18:05:10 +0300

    добавляет решение первой задачи

--- js/task-1-02.js
+++ js/task-1-02.js
@@ -1,12 +1,30 @@
-const logItems = function(array) {
-    for(let i=0; i<logItems.length -1; i +=1) {
-        console.log(logItems[i])
+const getItemsString = function(array) {
+    'use strict'; 
+    // Write code under this 
+
+    let result = '';
+    for(let i=0; i < array.length; i +=1) {  
+      
+      result += `${i + 1} - ${array[i]}\n`;
     }
-  };
+
+    return  result;
+  }
   
-  /*
-   * Вызовы функции для проверки работоспособности твоей реализации.
-   */
-  logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
   
-  logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+ console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+  
+//   '1 - Mango
+//   2 - Poly
+//   3 - Ajax
+//   4 - Lux
+//   5 - Jay
+//   6 - Kong
+//   '
+   
+ console.log(getItemsString([5, 10, 15]));
+  
+//   '1 - 5
+//   2 - 10
+//   3 - 15
+//   '
