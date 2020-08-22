44dd874d05f504584dde46f40ca7ebc7fe7131b7
Pavlenko-Oksana <65184570+Pavlenko-Oksana@users.noreply.github.com>
2020-08-22 11:50:24 +0300

    добавляет решение задачи 8

---
+++ js/task-5-02.js
@@ -0,0 +1,14 @@
+function checkForSpam (str) { 
+    'use strict';
+    const strings = str.split('').toLowerCase;
+    let message = '';
+    
+  }
+  
+  //console.log(checkForSpam('Latest technology news')); // false
+  
+  //console.log(checkForSpam('JavaScript weekly newsletter')); // false
+  
+  //console.log(checkForSpam('Get best sale offers now!')); // true
+  
+  //console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
